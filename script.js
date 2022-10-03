2eac42a7d791eb7717c4deb8aff023490008edc5
Abdulqadir <abdulqadirlalparia@gmail.com>
2022-10-03 19:23:52 +0530

    Completed creating grid function and it's button

--- script.js
+++ script.js
@@ -4,11 +4,14 @@ function creategrid(GridSize){
     let column;
     const container = document.getElementById('container');
 
+
+    // Appending columns of container
     for(let i = 0; i < (GridSize); i++){
     column = document.createElement('div');
     column.classList = "column";
     container.appendChild(column);
 
+    // Appending rows of container
     for(let j = 0; j < GridSize; j++){
         row = document.createElement('div');
         row.classList = "row";
@@ -17,4 +20,27 @@ function creategrid(GridSize){
     }
 
 }
-creategrid(16);
+
+function DeclareSize(creategrid){
+    // Button Functions
+let button;
+button = document.getElementById('prompt');
+
+button.onclick = () => {
+    
+    // Ask user for Size of grid.
+    const x = parseInt(prompt('Enter a size:'));
+
+    if(x > 100){
+        alert('Please enter a number below 100');
+        return 0;
+    }
+    // Clearing the container and creating a new one accoriding to user.
+    document.getElementById('container').textContent = "";
+    creategrid(x);
+}
+
+
+}
+
+DeclareSize(creategrid);
